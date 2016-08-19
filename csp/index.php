b087519661adf34a93b63dff269ddbec42ad5299
Adam Cox <mr.adamcox@gmail.com>
2016-08-19 16:38:26 -0500

    syntax fix

--- csp/index.php
+++ csp/index.php
@@ -42,7 +42,7 @@
             </div>
             <div class="col-xs-12">
                 <p>Participants: Please click <a href="http://opengeo.legiongis.com/geoexplorer/composer/#maps/4">here</a> to access the survey interface.</p>
-                <?php include 'map.php';?>
+                <?php include ('map.php');?>
             </div>
             
         </div>
